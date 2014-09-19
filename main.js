bf8c745da6b56d3c588e0315aa8d762017aee1f8
Cally Nhieu <calvin.nhieu@gmail.com>
2014-09-19 14:10:24 -0400

    fixed upload error

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ define(function (require, exports, module) {
     'use strict';
 
     var CommandManager =brackets.getModule("command/CommandManager");
-    var Menus          = brackets.getModule("command/Menus");
     var KeyBindingManager= brackets.getModule('command/KeyBindingManager');
 
     var OPEN_BEM_ID = "dderaedt.tutorialExt.LogHelloWorld";
@@ -15,7 +14,4 @@ define(function (require, exports, module) {
     KeyBindingManager.addBinding(OPEN_BEM_ID,"Alt-Shift-O");
 
     CommandManager.register(OPEN_BEM_NAME, OPEN_BEM_ID, openBEM);
-
-    var fileMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    fileMenu.addMenuItem(COMMAND_ID);
 });
